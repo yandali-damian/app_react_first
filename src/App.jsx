a8816b4f4fd9bf3962b09131e905ee87880d87c4
yandali <ydamianelguera@gmail.com>
2022-01-30 16:07:02 -0500

    add componet App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//importacion
+import React from 'react'
+
+//generacion de la funcion de componentes
+const App = () => {
+    //lo que ejecuta la funcion
+    console.log('hola consola')
+    return <h1>Calc App</h1>
+}
+
+//exportacion
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom' /* para tque se visualice en el navegador */
+//importar el componente
+import App from './App'
+
+//utilizar el componente
+ReactDOM.render(<App/>,
+    document.getElementById('root'))
+
